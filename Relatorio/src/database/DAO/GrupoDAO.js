c3fa0a6c8e3b53d7e13001ebb017b5eee7685ba8
Odesile Moreira <dragonildo221@gmail.com>
2025-12-18 02:55:35 -0300

    Adicionado o metodo read no GrupoDAO

--- Relatorio/src/database/DAO/GrupoDAO.js
+++ Relatorio/src/database/DAO/GrupoDAO.js
@@ -10,19 +10,42 @@ export class GrupoDao
      */
 
     async criar(grupo)
-    {
+    {   //Tenta fazer o insert da tabela grupo
         try
         {
+            //constante que guardará o comando sql
             const sql = 'INSERT INTO grupo (empresa, numero, nome) VALUES (?, ?, ?)'; //Placeholders
+            
+            //Os valores dos placeholders do comando sql
             const valores = [grupo.getempresa(), grupo.getnumero(). grupo.getnome()];
 
-            const  [resultado] = await pool.execute(sql, valores);// INSERT INTO grupo (empresa, numero, nome) VALUES (enoresa, numero, nome)
+            //Execulta o comando sql
+            const  [resultado] = await pool.execute(sql, valores);// INSERT INTO grupo (empresa, numero, nome) VALUES (empresa, numero, nome)
         
             return resultado.affectedRows > 0; // True ou False
         }
         catch(error)
         {
             console.log("Erro ao criar o grupo: ", error);
+            throw error;
+        }
+    }
+
+    // =-=-=-=-=-=-=-= Tentativa da função SELECT =-==-=-=-=-=-=-=-=-=
+    async select(grupo)
+    {
+        try
+        {
+            const sql_select = 'SEELECT * from ?';
+            const valor_select = grupo;
+            const [resultado_select] = await pool.execute(sql_select, valor_select);
+            
+            return resultado_select;
+            // SELECT * FROM grupo
+        }
+        catch(error)
+        {
+            console.log("Erro ao mostrar a tabela grupo")
             throw error;
         }
     }
